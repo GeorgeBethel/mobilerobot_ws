c7a4570356e435bb28272e40d5f4e3062338ec6f
GeorgeBethel <okoroaforgeorgebethel@gmail.com>
2020-03-06 01:04:11 +0100

    mobilerobot_ws

---
+++ build/mobile_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/george/mobilerobot_ws/src/mobile_robot_description
+# Build directory: /home/george/mobilerobot_ws/build/mobile_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/mobile_robot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/george/mobilerobot_ws/src/mobile_robot_gazebo
+# Build directory: /home/george/mobilerobot_ws/build/mobile_robot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
